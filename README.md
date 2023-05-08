6bcbf6b5353bc0b0f3d6e6ad3c08c2d1f0d3d133
Tymek Slomski <tymekslomski@gmail.com>
2023-05-08 21:09:59 +0200

    random bouncing of letters

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-Currently just a text writing animation.
-while in the process of creating it, I came up with an idea for animations of bouncing letters.
+Project animation of bouncing random letters of any words.
+
+This is an animation designed to animate strings. In the html file, we edit the p tag by adding any word of our choice. The letters are chosen randomly and are matched by the css-editable root value.
+
+In the future, he wants to skip jumping up blank spaces and punctuation marks
--- base.js
+++ base.js
@@ -1,5 +1,18 @@
 const text = document.querySelector(".text");
+const strText = text.textContent;
+const spliText = strText.split("");
 
-text.addEventListener("animationend", () => {
-  text.innerHTML = "Hello, my name is Tymek.";
-});
+const jumping = () => {
+  let jump = Math.floor(Math.random() * spliText.length);
+  text.textContent = "";
+
+  for (let i = 0; i < spliText.length; i++) {
+    text.innerHTML += "<span>" + spliText[i] + "</span>";
+  }
+
+  const span = text.querySelectorAll("span")[jump];
+  span.classList.add("up");
+};
+
+addEventListener("load", jumping);
+text.addEventListener("animationend", jumping);
