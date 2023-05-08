d2181df497f0f74df24fdae0ed2fd2c208f24ac5
Tymek Slomski <tymekslomski@gmail.com>
2023-05-08 22:31:00 +0200

    hopping of characters and spaces is disabled

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Project animation of bouncing random letters of any words.
 
 This is an animation designed to animate strings. In the html file, we edit the p tag by adding any word of our choice. The letters are chosen randomly and are matched by the css-editable root value.
 
-In the future, he wants to skip jumping up blank spaces and punctuation marks
+punctuation marks such as: , . : ; and spaces are not animated. you can easily add more disabled characters in if, which is in the 20th line of the js code.
--- base.js
+++ base.js
@@ -1,6 +1,6 @@
 const text = document.querySelector(".text");
-const strText = text.textContent;
-const spliText = strText.split("");
+const spliText = text.textContent.split("");
+const index = [];
 
 const jumping = () => {
   let jump = Math.floor(Math.random() * spliText.length);
@@ -10,8 +10,27 @@ const jumping = () => {
     text.innerHTML += "<span>" + spliText[i] + "</span>";
   }
 
+  index.push(jump);
+  if (index.length > 3) {
+    index.shift();
+  }
+
   const span = text.querySelectorAll("span")[jump];
-  span.classList.add("up");
+  const spanValue = span.innerHTML;
+  if (
+    spanValue === "," ||
+    spanValue === " " ||
+    spanValue === "." ||
+    spanValue === ":" ||
+    spanValue === ";" ||
+    index[0] === index[1] ||
+    index[0] === index[2] ||
+    index[1] === index[2]
+  ) {
+    span.classList.add("none");
+  } else {
+    span.classList.add("up");
+  }
 };
 
 addEventListener("load", jumping);
